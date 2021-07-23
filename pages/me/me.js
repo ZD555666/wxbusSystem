fd8955630e9113bf53a97272bdbcbc83ab8467ad
gjwaa <1258046117@qq.com>
2021-07-24 00:25:27 +0800

    登录页
    登录

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,4 +1,5 @@
 Component({
+
   pageLifetimes: {
     show() {
       this.getTabBar().setData({
@@ -6,7 +7,36 @@ Component({
       });
     }
   },
+
   data: {
-    
+
   },
+
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+    attached: function () { },
+    moved: function () { },
+    detached: function () { },
+  },
+
+
+  methods: {
+    getUserInfo(e) {
+      console.log(e.detail);
+    },
+
+    onGetUserInfo(e) {
+      console.log(e.detail.userInfo);
+    },
+
+    toLogin:function(){
+      wx.navigateTo({
+        url: '../login/login',
+      })
+    }
+
+  },
+
+  
+
 })
