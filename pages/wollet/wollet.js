da36887a140543485c4e23b73d6733d2d55ea810
gjwaa <1258046117@qq.com>
2021-07-26 18:01:34 +0800

    钱包
    钱包

--- pages/wollet/wollet.js
+++ pages/wollet/wollet.js
@@ -1,23 +1,42 @@
-// pages/wollet/wollet.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    assetsNum: '888',
+    assetsHideNum: '888',
+    closedEye: '',
+    openEye: ''
+  },
+  
+  eyes(){
+    this.setData({
+      assetsNum: this.data.assetsNum==='***'?this.data.assetsHideNum:'***'
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
     if (wx.getStorageSync("loginUserInfo") == '') {
       wx.redirectTo({
         url: '../login/login',
       })
     }
+    this.queryBalance()
+  },
+
+  queryBalance(){
+    wx.request({
+      url: app.globalData.prefix+'/wx/queryBalance',
+      method:'POST',
+      data:{
+        opid: ''
+      }
+    })
   },
 
   /**
