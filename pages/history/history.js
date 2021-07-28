790b36ab4eb0a5614158df403613261b2d18eff7
ZD555666 <1449974614@qq.com>
2021-07-28 10:20:09 +0800

    1

--- pages/history/history.js
+++ pages/history/history.js
@@ -7,23 +7,14 @@ Component({
     }
   },
   data: {
-    dataList:[]
+    dataList:[],
+    page:1,
   },
   pageLifetimes: {
     show: function() {
+      var _this = this;
       // 页面被展示
-      wx.request({
-        url: 'http://localhost:8080/wx/getNews',
-        data:{
-          param:1
-        },
-        success:reps=>{
-          console.log(reps.data)
-          this.setData({
-            dataList:reps.data
-          })
-        }
-      })
+      _this.onRequest();
     },
     hide: function() {
       // 页面被隐藏
@@ -31,5 +22,50 @@ Component({
     resize: function(size) {
       // 页面尺寸变化
     }
+  },
+  //触底响应函数
+  
+  methods:{
+    onReachBottom: function() {
+      this.onBottom()
+     },
+
+     onBottom(){
+      var that = this;
+      (this.data.page)++;
+      that.onRequest();
+      console.log(123)
+    },
+    onRequest(){
+      var _this = this;
+      wx.request({
+        url: 'http://localhost:8080/wx/getNews',
+        data:{
+          param:_this.data.page
+        },
+        success:reps=>{
+         if(_this.data.page == 1){
+          _this.setData({
+        		dataList: reps.data,
+         	})
+         }else{
+            //获取原始列表
+            let dataList = _this.data.dataList;
+            //获取新列表
+            let arr = reps.data;
+            //新列表数据与原列表数据合并
+            let newArr = dataList.concat(arr);
+            _this.setData({
+              dataList: newArr,
+            })
+         } 
+        }
+      })
+    },
+ 
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    
   }
 })
