57b43e68dc400ae6015815db5c35a3ecf687f68d
gjwaa <1258046117@qq.com>
2021-08-04 22:20:05 +0800

    1
    1

--- app.js
+++ app.js
@@ -19,13 +19,8 @@ App({
     address: '',
     cityInfo: '',
     userInfo: null,
-<<<<<<< HEAD
     prefix: 'http://localhost:8080'
     // prefix: 'http://192.168.31.49:8080'
-=======
-    // prefix: 'http://localhost:8080'
-    prefix: 'http://192.168.31.49:8080'
->>>>>>> gjw
     // prefix: 'http://192.168.135.211:8080'
   }
 })
